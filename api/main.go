89e1520b22c5e637699fcfa0457d7af90f617459
Matthew Wisdom <matthewwisdom11@gmail.com>
2025-01-30 21:43:57 +0100

    Create main.go

---
+++ api/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"log"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	r := gin.Default()
+	r.MaxMultipartMemory = 8 << 20 // 8 MiB
+
+	r.Use((LoggerMiddleware()))
+
+	r.POST("/share-file", shareFileUpload)
+
+	r.Run(":8080")
+
+}
