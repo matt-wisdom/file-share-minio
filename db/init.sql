3c10382e2102c8fbff3265bfebb86276f0ba291a
Matthew Wisdom <matthewwisdom11@gmail.com>
2025-02-01 17:45:09 +0100

    Update models

--- db/init.sql
+++ db/init.sql
@@ -6,19 +6,20 @@ CREATE TABLE users (
 
 CREATE TABLE files (
     file_id SERIAL PRIMARY KEY,
-    object_name VARCHAR(64) NOT NULL,
-    file_name VARCHAR(32) NOT NULL,
+    object_name VARCHAR(255) NOT NULL,
+    file_name VARCHAR(255) NOT NULL,
+    file_size INT NOT NULL,
     owner_id INT NOT NULL,
     created_at TIMESTAMP DEFAULT NOW(), 
     FOREIGN KEY (owner_id) REFERENCES users(user_id) ON DELETE CASCADE
 );
 
 CREATE TABLE file_shares (
+    share_id SERIAL PRIMARY KEY,
     file_id INT NOT NULL,
     shared_with INT NOT NULL,
     shared_at TIMESTAMP DEFAULT NOW(),
     received_at TIMESTAMP,
-    PRIMARY KEY (file_id, shared_with),
     FOREIGN KEY (file_id) REFERENCES files(file_id) ON DELETE CASCADE,
     FOREIGN KEY (shared_with) REFERENCES users(user_id) ON DELETE CASCADE
 );
